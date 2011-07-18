4902eae31bda32e96978f7cab3645f5e21a7038d
Olaf Radicke <radicke@atix.de>
2011-07-18 15:32:27 +0200

    bugfix try.

--- modules.d/99comoonics-debug/handler-cmdline.sh
+++ modules.d/99comoonics-debug/handler-cmdline.sh
@@ -3,12 +3,12 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 
-echo [communics-debugging] handler cmdline \n
+echo "[communics-debugging] handler cmdline ..............\n"
 # info "[communics-debugging] handler cmdline \n"
 
-for((i=10; i>0; i--))
-do
-  echo "."
+# for((i=10; i>0; i--))
+# do
+#   echo "."
 #  info "."
   sleep 1
-done
+# done
