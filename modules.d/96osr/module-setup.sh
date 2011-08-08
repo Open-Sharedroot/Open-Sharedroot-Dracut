bdfeed6f0d0f79e84fdd63d6a5ae9d21f880f53e
Olaf Radicke <radicke@atix.de>
2011-08-08 16:20:22 +0200

    bugfix

--- modules.d/96osr/module-setup.sh
+++ modules.d/96osr/module-setup.sh
@@ -75,6 +75,7 @@ install() {
     inst_simple "/usr/bin/igawk" /usr/bin
     chmod 755 "$moddir/lib/setup-osrenv.awk"
     inst_script "$moddir/lib/setup-osrenv.awk" /lib/osr
+    inst_simple "$moddir/lib/lib-repository.awk" /lib/osr
     inst_simple "$moddir/lib/lib-std.awk" /lib/osr
     # AWK-Hook-Wrapper
     inst_hook cmdline  1  "$moddir/hook-setup-osrenv.sh"
